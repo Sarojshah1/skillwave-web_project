394039a5bdf99eda5004917198f3ab9f8529d85b
saroj kumar sah <sarojahah152@hmail.com>
2025-07-08 18:33:23 +0545

    feat: add useVerifyOtp hook

---
+++ src/features/shared_features/auth/hooks/useVerifyOtp.js
@@ -0,0 +1,8 @@
+import { useMutation } from '@tanstack/react-query';
+import { authService } from '../services/authService';
+
+export const useVerifyOtp = () => {
+  return useMutation({
+    mutationFn: ({ email, otp }) => authService.verifyOtp(email, otp),
+  });
+}; 
