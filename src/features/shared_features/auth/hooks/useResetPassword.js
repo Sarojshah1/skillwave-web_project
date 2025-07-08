fc405d468e1f758e8b77fcb834aacb3da5b508d0
saroj kumar sah <sarojahah152@hmail.com>
2025-07-08 18:32:04 +0545

    feat: add useResetPassword hook

---
+++ src/features/shared_features/auth/hooks/useResetPassword.js
@@ -0,0 +1,8 @@
+import { useMutation } from '@tanstack/react-query';
+import { authService } from '../services/authService';
+
+export const useResetPassword = () => {
+  return useMutation({
+    mutationFn: ({ email, newPassword }) => authService.updatePasswordByEmail(email, newPassword),
+  });
+}; 
