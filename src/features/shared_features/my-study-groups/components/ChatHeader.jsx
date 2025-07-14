2d0524064dd41156a659253fe18adec9724ab408
saroj kumar sah <sarojahah152@hmail.com>
2025-07-14 17:26:14 +0545

    fix: update ChatHeader component

--- src/features/shared_features/my-study-groups/components/ChatHeader.jsx
+++ src/features/shared_features/my-study-groups/components/ChatHeader.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Video, Phone, Users, MoreVertical, PhoneOff } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
